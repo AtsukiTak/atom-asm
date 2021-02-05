1331eb918d97d2006054d0b8c08973d187a82e7d
AtsukiTak <takatomgoo@gmail.com>
2021-02-05 18:52:04 +0900

    ncmds, size_of_cmds, flagsをパースできるように

--- macho-parser/src/header.rs
+++ macho-parser/src/header.rs
@@ -1,12 +1,16 @@
 use crate::Magic;
 use mach_object as macho;
 use num_derive::FromPrimitive;
+use num_traits::FromPrimitive as _;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Header {
     pub magic: Magic,
     pub cpu_type: CpuType,
     pub file_type: FileType,
+    pub n_cmds: u32,
+    pub size_of_cmds: u32,
+    pub flags: Vec<Flag>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -28,14 +32,19 @@ pub enum CpuSubTypeX86_64 {
 impl CpuType {
     pub fn from_i32_i32(cpu_type_n: i32, cpu_subtype_n: i32) -> Self {
         match cpu_type_n {
+            // x86
             n if n == macho::CPU_TYPE_X86 => match cpu_subtype_n {
                 n if n == macho::CPU_SUBTYPE_X86_ALL => CpuType::X86(CpuSubTypeX86::All),
                 _ => panic!("Unsupported cpu_subtype : {}", cpu_subtype_n),
             },
+
+            // x86_64
             n if n == macho::CPU_TYPE_X86_64 => match cpu_subtype_n {
                 n if n == macho::CPU_SUBTYPE_X86_64_ALL => CpuType::X86_64(CpuSubTypeX86_64::All),
                 _ => panic!("Unsupported cpu_subtype : {}", cpu_subtype_n),
             },
+
+            // others
             _ => panic!("Unsupported cpu_type : {}", cpu_type_n),
         }
     }
@@ -51,4 +60,35 @@ pub enum FileType {
     Core = macho::MH_CORE as isize,
     Dylinker = macho::MH_DYLINKER as isize,
     Dsym = macho::MH_DSYM as isize,
+}
+
+#[derive(FromPrimitive, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Flag {
+    NoUndefs = macho::MH_NOUNDEFS as isize,
+    IncrLink = macho::MH_INCRLINK as isize,
+    DyldLink = macho::MH_DYLDLINK as isize,
+    TwoLevel = macho::MH_TWOLEVEL as isize,
+    BindAtLoad = macho::MH_BINDATLOAD as isize,
+    PreBound = macho::MH_PREBOUND as isize,
+    PreBindable = macho::MH_PREBINDABLE as isize,
+    NoFixPreBinding = macho::MH_NOFIXPREBINDING as isize,
+    AllModsBound = macho::MH_ALLMODSBOUND as isize,
+    Canonical = macho::MH_CANONICAL as isize,
+    SplitSegs = macho::MH_SPLIT_SEGS as isize,
+    ForceFlat = macho::MH_FORCE_FLAT as isize,
+    SubsectionsViaSymbols = macho::MH_SUBSECTIONS_VIA_SYMBOLS as isize,
+    NoMultiDefs = macho::MH_NOMULTIDEFS as isize,
+}
+
+impl Flag {
+    pub fn vec_from_u32(flags: u32) -> Vec<Flag> {
+        let mut vec = Vec::new();
+        for i in 0..=31 {
+            if flags & (1 << i) != 0 {
+                let flag = Flag::from_u32(1 << i).expect("Invalid flag");
+                vec.push(flag);
+            }
+        }
+        vec
+    }
 }
--- macho-parser/src/parser.rs
+++ macho-parser/src/parser.rs
@@ -1,4 +1,4 @@
-use crate::{CpuType, FileType, Header, MachO, Magic};
+use crate::{CpuType, FileType, Flag, Header, MachO, Magic};
 use byteorder::{ByteOrder, NativeEndian, ReadBytesExt};
 use num_traits::FromPrimitive as _;
 
@@ -14,14 +14,16 @@ pub fn parse<Buf: ReadBytesExt>(buf: &mut Buf) -> MachO {
         .expect("Unable to read magic number");
     let magic = Magic::from_u32(magic_n).expect("Invalid magic number");
 
-    match magic {
-        Magic::Magic64 => parse_inner::<Buf, NativeEndian>(buf, magic),
-        Magic::Cigam64 => parse_inner::<Buf, ReverseEndian>(buf, magic),
+    let header = match magic {
+        Magic::Magic64 => parse_header::<Buf, NativeEndian>(buf, magic),
+        Magic::Cigam64 => parse_header::<Buf, ReverseEndian>(buf, magic),
         _ => unimplemented!(),
-    }
+    };
+
+    MachO { header }
 }
 
-fn parse_inner<Buf: ReadBytesExt, Endian: ByteOrder>(buf: &mut Buf, magic: Magic) -> MachO {
+fn parse_header<Buf: ReadBytesExt, Endian: ByteOrder>(buf: &mut Buf, magic: Magic) -> Header {
     let cpu_type_n = buf.read_i32::<Endian>().expect("Unable to read cpu_type");
     let cpu_subtype_n = buf
         .read_i32::<Endian>()
@@ -31,11 +33,20 @@ fn parse_inner<Buf: ReadBytesExt, Endian: ByteOrder>(buf: &mut Buf, magic: Magic
     let file_type_n = buf.read_u32::<Endian>().expect("Unable to read file_type");
     let file_type = FileType::from_u32(file_type_n).expect("Invalid file_type number");
 
-    let header = Header {
+    let n_cmds = buf.read_u32::<Endian>().expect("Unable to read ncmds");
+    let size_of_cmds = buf
+        .read_u32::<Endian>()
+        .expect("Unable to read size_of_cmds");
+
+    let flags_int = buf.read_u32::<Endian>().expect("Unable to read flags");
+    let flags = Flag::vec_from_u32(flags_int);
+
+    Header {
         magic,
         cpu_type,
         file_type,
-    };
-
-    MachO { header }
+        n_cmds,
+        size_of_cmds,
+        flags,
+    }
 }
