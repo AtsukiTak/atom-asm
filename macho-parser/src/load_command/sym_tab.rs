5df3eb38822ef3339645e2654d6a07ea871c33eb
AtsukiTak <takatomgoo@gmail.com>
2021-02-12 23:11:09 +0900

    symtab_commandをパースできるように

--- macho-parser/src/load_command/sym_tab.rs
+++ macho-parser/src/load_command/sym_tab.rs
@@ -3,14 +3,26 @@ use crate::Buffer;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SymTab {
     cmd_size: u32,
+    sym_off: u32,
+    n_syms: u32,
+    str_off: u32,
+    str_size: u32,
 }
 
 impl SymTab {
     pub fn parse(buf: &mut Buffer) -> Self {
         let cmd_size = buf.read_u32();
+        let sym_off = buf.read_u32();
+        let n_syms = buf.read_u32();
+        let str_off = buf.read_u32();
+        let str_size = buf.read_u32();
 
-        buf.skip((cmd_size - 4 - 4) as usize);
-
-        SymTab { cmd_size }
+        SymTab {
+            cmd_size,
+            sym_off,
+            n_syms,
+            str_off,
+            str_size,
+        }
     }
 }
