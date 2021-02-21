681c687fcf471bc00c8eba26b831bf8ae9f44b84
AtsukiTak <takatomgoo@gmail.com>
2021-02-22 01:26:22 +0900

    BuildVersionをパースできるようになった

---
+++ macho-parser/src/load_command/build_version.rs
@@ -0,0 +1,116 @@
+use crate::Buffer;
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive as _;
+
+/// The build_version_command contains the min OS version on which this
+/// binary was built to run for its platform.  The list of known platforms and
+/// tool values following it.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct BuildVersion {
+    /// sizeof(BuildVersion) + ntools * sizeof(BuildToolVersion)
+    cmd_size: u32,
+    platform: Platform,
+    minos: Version,
+    sdk: Version,
+    ntools: u32,
+
+    tools: Vec<BuildToolVersion>,
+}
+
+impl BuildVersion {
+    pub fn parse(buf: &mut Buffer) -> Self {
+        let cmd_size = buf.read_u32();
+        let platform = Platform::parse(buf);
+        let minos = Version::parse(buf);
+        let sdk = Version::parse(buf);
+        let ntools = buf.read_u32();
+
+        let mut tools = Vec::with_capacity(ntools as usize);
+        for _ in 0..ntools {
+            tools.push(BuildToolVersion::parse(buf));
+        }
+
+        // 必ず8バイト境界にそろうためアライメントは不要
+
+        BuildVersion {
+            cmd_size,
+            platform,
+            minos,
+            sdk,
+            ntools,
+            tools,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, FromPrimitive)]
+pub enum Platform {
+    MacOS = 1,
+    IOS = 2,
+    TvOS = 3,
+    WatchOS = 4,
+    BridgeOS = 5,
+    MacCatalyst = 6,
+    IOSSimulator = 7,
+    TvOSSimulator = 8,
+    WatchOSSimulator = 9,
+    Driverkit = 10,
+}
+
+impl Platform {
+    fn parse(buf: &mut Buffer) -> Self {
+        let platform_n = buf.read_u32();
+        Platform::from_u32(platform_n)
+            .unwrap_or_else(|| panic!("Invalid platform number {}", platform_n))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Version {
+    major: u16,
+    minor: u8,
+    release: u8,
+}
+
+impl Version {
+    fn parse(buf: &mut Buffer) -> Self {
+        let n = buf.read_u32();
+        let major = (n >> 16) as u16;
+        let minor = ((n >> 8) & 0xFF) as u8;
+        let release = (n & 0xFF) as u8;
+        Version {
+            major,
+            minor,
+            release,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct BuildToolVersion {
+    tool: Tool,
+    version: u32,
+}
+
+impl BuildToolVersion {
+    fn parse(buf: &mut Buffer) -> Self {
+        let tool = Tool::parse(buf);
+        let version = buf.read_u32();
+
+        BuildToolVersion { tool, version }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, FromPrimitive)]
+pub enum Tool {
+    Clang = 1,
+    Swift = 2,
+    LD = 3,
+}
+
+impl Tool {
+    fn parse(buf: &mut Buffer) -> Self {
+        let tool_n = buf.read_u32();
+        Tool::from_u32(tool_n).unwrap_or_else(|| panic!("Unsupported tool number {}", tool_n))
+    }
+}
--- macho-parser/src/load_command/mod.rs
+++ macho-parser/src/load_command/mod.rs
@@ -1,6 +1,8 @@
+mod build_version;
 mod segment_64;
 mod sym_tab;
 
+pub use self::build_version::{BuildToolVersion, BuildVersion};
 pub use self::segment_64::{Section64, SectionAttr, SectionAttrs, SectionType, Segment64};
 pub use self::sym_tab::SymTab;
 
@@ -10,20 +12,23 @@ use crate::Buffer;
 pub enum LoadCommand {
     Segment64(Segment64),
     SymTab(SymTab),
+    BuildVersion(BuildVersion),
 }
 
 impl LoadCommand {
     const LC_SEGMENT_64: u32 = 0x19;
     const LC_SYMTAB: u32 = 0x02;
+    const LC_BUILD_VERSION: u32 = 0x32;
 
     pub fn parse(buf: &mut Buffer) -> Self {
+        use LoadCommand as LC;
+
         let cmd_type_n = buf.read_u32();
-        if cmd_type_n == Self::LC_SEGMENT_64 {
-            LoadCommand::Segment64(Segment64::parse(buf))
-        } else if cmd_type_n == Self::LC_SYMTAB {
-            LoadCommand::SymTab(SymTab::parse(buf))
-        } else {
-            panic!("Unsupported cmd_type 0x{:X}", cmd_type_n);
+        match cmd_type_n {
+            Self::LC_SEGMENT_64 => LC::Segment64(Segment64::parse(buf)),
+            Self::LC_SYMTAB => LC::SymTab(SymTab::parse(buf)),
+            Self::LC_BUILD_VERSION => LC::BuildVersion(BuildVersion::parse(buf)),
+            _ => panic!("Unsupported cmd_type 0x{:X}", cmd_type_n),
         }
     }
 }
