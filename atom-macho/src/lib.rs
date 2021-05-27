5ae8cf239d2419e217c7405322e21123b0fc1aa7
AtsukiTak <takatomgoo@gmail.com>
2021-05-27 12:28:21 +0900

    remove macho module

--- atom-macho/src/lib.rs
+++ atom-macho/src/lib.rs
@@ -1,7 +1,6 @@
 pub mod header;
 mod io;
 pub mod load_command;
-mod macho;
 pub mod nlist;
 pub mod reloc;
 pub mod string_table;
--- atom-macho/src/macho.rs
+++
@@ -1,7 +0,0 @@
-use crate::{header::Header64, load_command::LoadCommand};
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct MachO {
-    pub header: Header64,
-    pub load_commands: Vec<LoadCommand>,
-}
