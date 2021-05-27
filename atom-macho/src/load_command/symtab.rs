bb7cfb69be87d4836d6989b82e497fc22a750433
AtsukiTak <takatomgoo@gmail.com>
2021-05-28 00:43:34 +0900

    SymtabCommand::read_from_in, write_intoの実装

--- atom-macho/src/load_command/symtab.rs
+++ atom-macho/src/load_command/symtab.rs
@@ -1,3 +1,6 @@
+use crate::io::{Endian, ReadExt as _, WriteExt as _};
+use std::io::{Read, Write};
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SymtabCommand {
     pub cmd: u32,
@@ -16,12 +19,63 @@ pub struct SymtabCommand {
 impl SymtabCommand {
     pub const TYPE: u32 = 0x2;
 
-    #[rustfmt::skip]
-    pub const SIZE: u32 =
-        4       // cmd
-        + 4     // cmdsize
-        + 4     // symoff
-        + 4     // nsyms
-        + 4     // stroff
-        + 4; // strsize
+    pub const SIZE: u32 = 0x18; // 24
+
+    pub fn read_from_in<R: Read>(read: &mut R, endian: Endian) -> Self {
+        let cmd = read.read_u32_in(endian);
+        assert_eq!(cmd, SymtabCommand::TYPE);
+
+        let cmdsize = read.read_u32_in(endian);
+        assert_eq!(cmdsize, SymtabCommand::SIZE);
+
+        let symoff = read.read_u32_in(endian);
+        let nsyms = read.read_u32_in(endian);
+        let stroff = read.read_u32_in(endian);
+        let strsize = read.read_u32_in(endian);
+
+        SymtabCommand {
+            cmd,
+            cmdsize,
+            symoff,
+            nsyms,
+            stroff,
+            strsize,
+        }
+    }
+
+    pub fn write_into<W: Write>(&self, write: &mut W) {
+        write.write_u32_native(self.cmd);
+        write.write_u32_native(self.cmdsize);
+        write.write_u32_native(self.symoff);
+        write.write_u32_native(self.nsyms);
+        write.write_u32_native(self.stroff);
+        write.write_u32_native(self.strsize);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn write_and_read_symtab_command() {
+        let cmd = SymtabCommand {
+            cmd: SymtabCommand::TYPE,
+            cmdsize: SymtabCommand::SIZE,
+            symoff: 42,
+            nsyms: 1,
+            stroff: 100,
+            strsize: 7,
+        };
+
+        let mut buf = Vec::new();
+
+        cmd.write_into(&mut buf);
+
+        assert_eq!(buf.len(), SymtabCommand::SIZE as usize);
+
+        let read_cmd = SymtabCommand::read_from_in(&mut buf.as_slice(), Endian::NATIVE);
+
+        assert_eq!(read_cmd, cmd);
+    }
 }
