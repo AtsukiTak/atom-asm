f1b8a0877ab3acecee109955476b4cb616a96b8e
AtsukiTak <takatomgoo@gmail.com>
2021-06-28 22:55:00 +0900

    サンプルアセンブリコードを変更

--- atom-objparse/samples/static.s
+++ atom-objparse/samples/static.s
@@ -1,10 +1,18 @@
 section .text
 global start
-
 start:
   mov rax, 0x2000000 + 4    ;sys_write
   mov rdi, 1
-  mov rsi, msg
+
+  ;PC relative な命令を試す.
+  ;lea reg, [exp] 命令はexpの結果をそのままregに代入する.
+  ;mov reg, [exp] 命令はexpの結果のアドレスの値をregに代入する.
+  ;mov reg, imm 命令はimm値をregに代入する.
+  ;つまり、`lea rsi, [rel msg]` と `mov rsi, msg` は等価.
+  ;ただし、Mach-O（nasmで？）`mov rsi, msg` の形式が使える
+  ;ようになったのは割と最近（2020年くらいから？）ぽい
+  lea rsi, [rel msg]
+  ;こっちは mov reg, imm の形式
   mov rdx, len
   syscall
 
@@ -15,3 +23,7 @@ start:
 section .data
   msg db  'hello, world',0x0A
   len equ $ - msg
+
+;bss section を試す
+section .bss
+  hoge resb 42
